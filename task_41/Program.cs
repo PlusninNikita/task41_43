be70c8656d1aae78d80f56923bc970009edb156d
NikitaPlusnin <plusnin@mail.com>
2023-09-12 21:14:45 +0500

    task 41 and 43

---
+++ task_41/Program.cs
@@ -0,0 +1,67 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+//1. Получаю размер массива (с проверкой не менее 0)
+//2. Формирую алгоритм который будет создавать массив с размером N
+//3. Формирую цикл который будет создавать N пользовательских запросов
+//4. Считаю сколько элементов в массиве меньше 0.
+
+int[] RequestUserArray()
+{
+    int size = 0;
+    Console.WriteLine("Создание нового массива данных");
+    Console.Write("Введите размер массива: ");
+    size = Convert.ToInt32(Console.ReadLine());
+    while (size < 1)
+    {
+        Console.WriteLine("Ошибка: размер должен быть больше нуля!");
+        Console.Write("Введите размер массива: ");
+        size = Convert.ToInt32(Console.ReadLine());
+    }
+    int[] arr = new int[size];
+    return arr;
+}
+
+void ManuallyFillArray (int[] arr)
+{
+    Console.WriteLine($"Заполните массив натуральными числами {arr.Length} раз");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"Элемент {1+i} из {arr.Length}: ");
+        arr[i] = Convert.ToInt32(Console.ReadLine());  
+    }
+}
+
+int CountNegativeElement(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i] < 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for(int i = 0; i < arr.Length; i++)
+    {
+        if(i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.Write("]");
+}
+
+int[] array = RequestUserArray();
+ManuallyFillArray(array);
+Console.WriteLine();
+PrintArray(array);
+int negativeElement = CountNegativeElement(array);
+Console.WriteLine();
+Console.WriteLine($"Количество элементов в массиве меньше нуля: {negativeElement} шт.");
---
+++ task_43/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 43. Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+double[] IntersectionTwoLines(double k1, double b1, double k2, double b2)
+{
+    double[] pos = new double[2];
+    var x = -(b1 - b2) / (k1 - k2);
+    var y = k1 * x + b1;
+    pos[0] = x;
+    pos[1] = y;
+    return pos;
+}
+
+double[] InputeLinesData()
+{
+    Console.Write("Введите k1: ");
+    double k1 = Convert.ToDouble(Console.ReadLine());
+    Console.Write("Введите b1: ");
+    double b1 = Convert.ToDouble(Console.ReadLine());
+    Console.Write("Введите k2: ");
+    double k2 = Convert.ToDouble(Console.ReadLine());
+    Console.Write("Введите b2: ");
+    double b2 = Convert.ToDouble(Console.ReadLine());
+    double[] pos = IntersectionTwoLines(k1, b1, k2, b2);
+    return pos;
+}
+
+void PrintPosition(double[] pos)
+{
+    Console.Write("Точка пересечения: (");
+    for (int i = 0; i < pos.Length; i++)
+    {
+        if (i < pos.Length - 1) Console.Write($"{pos[i]}, ");
+        else Console.Write($"{pos[i]}");
+    }
+    Console.Write(")");
+}
+
+double[] position = InputeLinesData();
+PrintPosition(position);
